71d2f3cd99a055e2dc615e282dfac33e4e3273c3
ZJason5958 <zjason5958@gmail.com>
2025-04-07 20:02:07 -0400

    Create frame.

--- Pac-Man/src/App.java
+++ Pac-Man/src/App.java
@@ -1,5 +1,18 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int rows = 18;
+        int columns = 32;
+        int tileSize = 80;
+        int boardWidth = columns * tileSize;
+        int boardHeight = rows * tileSize;
+
+        JFrame frame = new JFrame("Pac-Man");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
