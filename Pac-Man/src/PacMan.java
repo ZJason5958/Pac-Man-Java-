45602b6b8ca58a372badc318a163fbad857dc048
ZJason5958 <zjason5958@gmail.com>
2025-04-13 17:27:22 -0400

    Renamed Pac-Man image files.

--- Pac-Man/src/PacMan.java
+++ Pac-Man/src/PacMan.java
@@ -14,9 +14,19 @@ public class PacMan extends JPanel {
     private Image orangeGhostImage;
     private Image pinkGhostImage;
     private Image redGhostImage;
+    private Image pacManUpImage;
+    private Image pacManDownImage;
+    private Image pacManLeftImage;
+    private Image pacManRightImage;
 
     PacMan() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.BLACK);
+        wallImage = new ImageIcon(getClass().getResource("./wall.png")).getImage();
+        blueGhostImage = new ImageIcon(getClass().getResource("./blueGhost.png")).getImage();
+        orangeGhostImage = new ImageIcon(getClass().getResource("./orangeGhost.png")).getImage();
+        pinkGhostImage = new ImageIcon(getClass().getResource("./pinkGhost.png")).getImage();
+        redGhostImage = new ImageIcon(getClass().getResource("./redGhost.png")).getImage();
+        pacManImage = new ImageIcon(getClass().getResource("./wall.png")).getImage();
     }
 }
